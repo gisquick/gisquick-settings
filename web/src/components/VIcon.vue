c5b0b1b45066408b64a6393723ba119006746d1c
Marcel Dancak <dancakm@gmail.com>
2019-09-05 00:23:57 +0200

    New icons/images

---
+++ web/src/components/VIcon.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    name: String
+  },
+  render (h, ctx) {
+    const attrs = {
+      'xlink:href': `#${ctx.props.name}`
+    }
+    const use = h('use', { attrs })
+    return h('svg', ctx.data, [use])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-icon {
+  fill: currentColor;
+}
+</style>
--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import 'material-icons/iconfont/material-icons.scss'
 import Vuetify from 'vuetify/lib'
 
+import VIcon from '@/components/VIcon.vue'
+
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        // primary: '#00b8e5'
+        primary: '#00a6e5'
+      }
+    }
+  },
   icons: {
-    iconfont: 'mdi',
+    values: {
+      qgis: {
+        component: VIcon,
+        props: {
+          name: 'qgis'
+        }
+      }
+    }
   },
 })
