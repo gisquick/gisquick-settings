a7c3dbd9abea197207e452e212dc5a58d957820d
Marcel Dancak <dancakm@gmail.com>
2020-01-20 15:43:34 +0100

    Added note about required update of plugin from old version

--- web/src/components/PluginDisconnected.vue
+++ web/src/components/PluginDisconnected.vue
@@ -2,7 +2,6 @@
   <v-layout column align-center justify-center>
     <h4 class="my-4">QGIS plugin is not connected!</h4>
     <img src="@/assets/qgis_disconnected.svg" class="my-2">
-    <!-- <small class="my-4">* A special version of plugin is required, which is not available yet.</small> -->
     <small class="mt-5 mb-2">You can download required plugin here (64-bit OS):</small>
     <v-layout shrink>
       <v-btn href="/media/files/gisquick2_lin64.zip" text>
@@ -15,6 +14,10 @@
         <icon name="apple"/>
       </v-btn>
     </v-layout>
+    <small class="orange--text mt-3">
+      <v-icon small color="orange" class="mr-1 mb-1">warning</v-icon>
+      <span>After changes in API, previous version will not work (2.0.0). Update to the latest version.</span>
+    </small>
   </v-layout>
 </template>
 
@@ -32,6 +35,7 @@ h4 {
 small {
   color: #777;
   font-weight: 500;
+  text-align: center;
 }
 img {
   width: 600px;
