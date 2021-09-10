89bc5acd235edfc6df17707467bd7ee0497bbb23
Marcel Dancak <dancakm@gmail.com>
2021-09-10 12:51:54 +0200

    web: added websocket reconnection support and logs for debugging

--- web/src/ws.js
+++ web/src/ws.js
@@ -3,7 +3,7 @@ export default function WebsocketMessenger (url) {
   let listeners = []
   let openListeners = []
   const activeRequests = {}
-  const socket = new WebSocket(url)
+  let socket = new WebSocket(url)
   let timer = null
   const ws = {
     connected: false,
@@ -18,8 +18,12 @@ export default function WebsocketMessenger (url) {
       listeners = listeners.filter(l => l.type !== type || l.callback !== callback)
     },
     send (name, data) {
-      const msg = { type: name, data }
-      socket.send(JSON.stringify(msg))
+      if (socket.readyState === socket.OPEN) {
+        const msg = { type: name, data }
+        socket.send(JSON.stringify(msg))
+      } else {
+        console.log('ws:send readyState', socket.readyState)
+      }
     },
     request (name, data) {
       return new Promise((resolve, reject) => {
@@ -28,10 +32,6 @@ export default function WebsocketMessenger (url) {
       })
     },
     close () {
-      if (timer !== null) {
-        clearInterval(timer)
-        timer = null
-      }
       socket.close()
     },
     onopen () {
@@ -45,35 +45,53 @@ export default function WebsocketMessenger (url) {
     }
   }
 
-  socket.onopen = () => {
-    ws.connected = true
-    openListeners.forEach(cb => cb())
-    openListeners = []
-    ws.send('PluginStatus')
-  }
-  socket.onclose = () => {
-    ws.connected = false
-  }
-  socket.onmessage = (e) => {
-    const msg = JSON.parse(e.data)
-    if (msg.type === 'PluginStatus') {
-      const connected = msg.status === 200
-      ws.pluginConnected = connected
-      ws.clientInfo = connected && msg.data.client
+  const init = () => {
+    socket.onopen = () => {
+      ws.connected = true
+      openListeners.forEach(cb => cb())
+      openListeners = []
+      ws.send('PluginStatus')
+      timer = setInterval(() => {
+        ws.send('PluginStatus')
+      }, 30 * 1000)
     }
-
-    if (activeRequests[msg.type]) {
-      if (msg.status && msg.status >= 400) {
-        activeRequests[msg.type].reject(msg)
-      } else {
-        activeRequests[msg.type].resolve(msg)
+    socket.onclose = () => {
+      console.log('ws.onclose')
+      ws.connected = false
+      if (timer !== null) {
+        clearInterval(timer)
+        timer = null
       }
-      delete activeRequests[msg.type]
+      // reconnect
+      setTimeout(() => {
+        console.log('ws:reconnecting...')
+        socket = new WebSocket(url)
+        init()
+      }, 5000)
     }
-    listeners.filter(l => l.type === msg.type).forEach(l => l.callback(msg))
+    socket.onmessage = (e) => {
+      const msg = JSON.parse(e.data)
+      if (msg.type === 'PluginStatus') {
+        const connected = msg.status === 200
+        ws.pluginConnected = connected
+        ws.clientInfo = connected && msg.data.client
+      }
+
+      if (activeRequests[msg.type]) {
+        if (msg.status && msg.status >= 400) {
+          activeRequests[msg.type].reject(msg)
+        } else {
+          activeRequests[msg.type].resolve(msg)
+        }
+        delete activeRequests[msg.type]
+      }
+      listeners.filter(l => l.type === msg.type).forEach(l => l.callback(msg))
+    }
+    socket.onerror = (e) => {
+      console.log('ws.onerror', 'readyState:', socket.readyState, e)
+      // socket.close()
+    }
   }
-  timer = setInterval(() => {
-    ws.send('PluginStatus')
-  }, 30 * 1000)
+  init()
   return ws
 }
